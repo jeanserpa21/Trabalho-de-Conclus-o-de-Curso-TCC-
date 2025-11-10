b5dada326e47863242eaa38a658e6749d2a382ad
jeanserpa21 <140073864+jeanserpa21@users.noreply.github.com>
2025-11-09 23:29:35 -0300

    Delete Código-Fonte directory

--- Código-Fonte/tests/test-hash.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcrypt');
-
-const senhaDigitada = 'senha123';
-const hashDoBanco = 'COLE_O_HASH_AQUI'; // Cole o hash que você copiou
-
-bcrypt.compare(senhaDigitada, hashDoBanco, (err, result) => {
-  console.log('Senha correta?', result);
-});
